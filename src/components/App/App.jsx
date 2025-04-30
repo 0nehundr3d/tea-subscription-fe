6b3d2d8d0b15c60e56249b1b43dcf4ac664a0063
Sebastian McKee <sebmcke@gmail.com>
2025-04-30 17:18:19 -0500

    Add missing page functionality to website

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import "./App.css"
 import Home from "../home/home"
 import SubscriptionDetails from "../SubscriptionDetails/SubscriptionDetails"
 import { Routes, Route } from "react-router-dom"
+import NotFound from "../NotFound/NotFound"
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/subscriptions/:id" element={<SubscriptionDetails />} />
+        <Route path="/subscriptions/not_found" element={<NotFound />} />
       </Routes>
     </section>
   )
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import "./NotFound.css"
+
+function NotFound() {
+    return (
+        <h2>The Subscription you were looking for could not be found :(</h2>
+    )
+}
+
+export default NotFound
